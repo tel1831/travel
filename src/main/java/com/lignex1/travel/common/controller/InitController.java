085b2bf3988b090bc823db803d7438ec8ba5fad6
tel1831 <tel1831@gmail.com>
2025-05-25 17:48:09 +0900

    sch 1

--- src/main/java/com/lignex1/travel/common/controller/InitController.java
+++ src/main/java/com/lignex1/travel/common/controller/InitController.java
@@ -44,7 +44,6 @@ public class InitController {
 	@RequestMapping(value = "/popup", method = RequestMethod.GET)
 	public String IndexWebSquare(HttpServletRequest request, Model model) throws Exception {
 		log.info("/popup w2xPath={}", request.getParameter("w2xPath"));
-		log.info("second log");
 		model.addAttribute("movePage", loginService.getLoginPage(request.getParameter("w2xPath")));
 		return "websquare/popup";
 	}
