85264e2ecc911f8512b769d2a91852ad2b246463
tel1831 <tel1831@gmail.com>
2025-05-25 17:50:25 +0900

    main 1

--- src/main/java/com/lignex1/travel/controller/TestController.java
+++ src/main/java/com/lignex1/travel/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.lignex1.travel.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequestMapping("/test")
 public class TestController {
 
+	
 }
